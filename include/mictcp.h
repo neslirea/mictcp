5e10ebb07dbb346e073f04d50f40fbbf7fce928b
Aubery Clea <aubery@insa-20714>
2022-03-16 16:10:55 +0100

    tag

--- include/mictcp.h
+++ include/mictcp.h
@@ -1,4 +1,4 @@
-#ifndef MICTCP_H
+XT#ifndef MICTCP_H
 #define MICTCP_H
 
 #include <stdio.h>
@@ -47,7 +47,7 @@ typedef struct mic_tcp_sock
 {
   int fd;  /* descripteur du socket */
   protocol_state state; /* état du protocole */
-  mic_tcp_sock_addr addr; /* adresse du socket */
+  mic_tcp_sock_addr; /* adresse du socket */
 } mic_tcp_sock;
 
 /*
